de11881466e0538fb5156d3791390ea126e5e3a3
ys <zhangyuesheng@kushimai.com>
2023-12-13 02:14:07 +0800

    🐯(basic_language_model_moe.py):添加MOE模型初始调试代码

--- quickllm/layers/moe.py
+++ quickllm/layers/moe.py
@@ -333,23 +333,3 @@ class HeirarchicalMoE(nn.Module):
         expert_outputs_outer = torch.einsum('efbcd,ebnfc->ebnd', expert_outputs, combine_tensor_inner)
         output = torch.einsum('ebcd,bnec->bnd', expert_outputs_outer, combine_tensor_outer)
         return output, (loss_outer + loss_inner) * self.loss_coef
-
-
-if __name__ == "__main__":
-
-    moe = MoE(
-        dim=512,  # 输入张量的维度
-        num_experts=16,  # 专家数量，可以增加该参数而不增加计算量
-        hidden_dim=512 * 4,  # 每个专家网络中的隐藏层维度，默认为 4 倍输入维度
-        activation=nn.LeakyReLU,  # 使用的激活函数，默认为 GELU
-        second_policy_train='random',  # 使用的第二名专家的训练策略
-        second_policy_eval='random',  # 使用的第二名专家的验证策略
-        second_threshold_train=0.2,  # 训练时使用的第二名专家阈值
-        second_threshold_eval=0.2,  # 测试时使用的第二名专家阈值
-        capacity_factor_train=1.25,  # 每个专家网络在单个批次中的固定容量，需要额外的容量以防门控不平衡
-        capacity_factor_eval=2.,  # capacity_factor_* 应设置为 >=1 的值
-        loss_coef=1e-2  # 辅助专家平衡辅助损失的乘数
-    )
-    inputs = torch.randn(4, 1024, 512)
-    out, aux_loss = moe(inputs)  # (4, 1024, 512), (1,)
-    print(out.shape, aux_loss.shape)
